0d1b2836822146c63b97cf43faf98f42d2c851db
Jack Scott <jack.scott10@gmail.com>
2025-02-14 07:52:16 +0000

    Updates to main page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
+            maxWidth: {
+                "8xl": "96rem"
+            },
             fontFamily: {
                 body: ["Inter", "Sans Serif"],
                 title: ["DM Sans", "Sans Serif"]
