c1462c760a641b79f1c8929fa180213501a69313
Jack Scott <jack.scott10@gmail.com>
2025-04-19 21:42:29 +0100

    Update social config

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,12 @@ import type { Site, SocialMediaObjects } from "./types";
 
 export const SITE: Site = {
     siteUrl: "https:/www.astro-lane.avenuelabs.co/", // Always put "/" at the end of the URL
-    author: "Jack Scott",
+    author: "Jack",
     desc: "A personal portfolio landing template for developers and designers. Made by Avenue Labs.",
     title: "jdlennoxs",
     ogImage: "images/astro-lane.png",
     keywords:
-        "Personal portfolio, landing page, page template, developer portfoliom designer portfolio",
+        "Personal portfolio, landing page, page template, developer portfolio, designer portfolio",
     postPerPage: 6
 };
 
@@ -18,35 +18,35 @@ export const SITE: Site = {
 export const SOCIALS: SocialMediaObjects = [
     {
         name: "Github",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/jdlennoxs",
         icon: "tabler:brand-github",
         title: `Follow ${SITE.author} on Github`,
         active: true
     },
     {
         name: "Instagram",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://www.instagram.com/jdlennoxs/",
         icon: "tabler:brand-instagram",
         title: `Follow ${SITE.author} on Instagram`,
         active: true
     },
     {
+        name: "YouTube",
+        href: "https://www.youtube.com/channel/UCAt5upYuUcdK3AfIXl1ORWQ",
+        icon: "tabler:brand-youtube",
+        title: `${SITE.title} on YouTube`,
+        active: false
+    },
+    {
         name: "LinkedIn",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://www.linkedin.com/in/jack-scott-2920ab47/",
         icon: "tabler:brand-linkedin",
         title: `Follow ${SITE.title} on LinkedIn`,
         active: true
     },
     {
-        name: "YouTube",
-        href: "https://github.com/christian-luntok/astro-lane/",
-        icon: "tabler:brand-youtube",
-        title: `${SITE.title} on YouTube`,
-        active: true
-    },
-    {
         name: "Bluesky",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://bsky.app/profile/jdlennoxs.bsky.social",
         icon: "tabler:brand-bluesky",
         title: `${SITE.title} on Bluesky`,
         active: true
